6165e6b3e74a04b4ac4e14e15b0ce487fefca6ef
Stephen <40372402+Stephen-Bitrus@users.noreply.github.com>
2022-10-31 19:16:25 +0100

    Fixed CORS Issues

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 
 
-const app = express()
+const app = express();
+
+app.use(cors());
 
 const port = 4397
 
@@ -17,3 +20,5 @@ app.get("/", (req, res)=>{
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`)
 })
+
+
