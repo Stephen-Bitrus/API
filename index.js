2498c4def18b8541baeaae2fef3d37da0317a401
Stephen <40372402+Stephen-Bitrus@users.noreply.github.com>
2022-10-31 18:43:27 +0100

    Convert array to object

--- index.js
+++ index.js
@@ -5,16 +5,13 @@ const app = express()
 
 const port = 4397
 
-const task1 = [
-    {"slackUsername": "BStephen",
-    "backend": true,
-    "age": 24,
-    "bio": "I'm curious, and I love the intellectual challenge of creatively overcoming limitations"},
-]
-
 
 app.get("/", (req, res)=>{
-    res.json(task1)
+    res.json(
+        {"slackUsername": "BStephen",
+        "backend": true,
+        "age": 24,
+        "bio": "I'm curious, and I love the intellectual challenge of creatively overcoming limitations"},)
 })
 
 app.listen(port, ()=>{
